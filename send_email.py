1e1e9081245afe0ef16feea51041c0da008716fb
Bgarcia117 <bryant.garcia.1771@gmail.com>
2023-06-20 08:59:00 -0500

    Created contact form and added functionality

---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib, ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "bryantg1703@gmail.com"
+    password = "ohrgrgshfaeojjjd"
+
+    receiver = "bryantg1703@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+    # The function expects arguments in order, but context is a few arguments down the list, so we have to be more specific
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+    
